9417e36fad731e63dd97e76c4e8bb5dbf0321402
tzraeq <tzraeq@gmail.com>
2016-02-05 12:58:26 +0000

    finish the table page

--- applications/tools/controllers/tenxcloud.py
+++ applications/tools/controllers/tenxcloud.py
@@ -5,7 +5,25 @@ import json
 from com.baoze.tenxcloud import api
 
 def index():
-    return dict(message="hello from tenxcloud.py")
+    regions = [];
+    regionsDict = api.getRegions()
+    if api.KEY_REGIONS in regionsDict:
+        for region in regionsDict[api.KEY_REGIONS]:
+            regionName = region[api.KEY_REGION_NAME]
+            servicesDict = api.getServices(str(regionName))
+            if api.KEY_SERVICES in servicesDict:
+                services = servicesDict[api.KEY_SERVICES];
+                if(len(services) > 0):
+                    regionObj = {"name":regionName,"displayName":region[api.KEY_REGION_DISPLAY_MAME],"services":[]}
+                    regions.append(regionObj)
+                    for service in services:
+                        serviceObj = {"name":service[api.KEY_SERVICE_NAME],"ports":[]}
+                        regionObj["services"].append(serviceObj)
+                        for portMapping in service[api.KEY_PORT_MAPPING]:
+                            pm = {"protocol":portMapping[api.KEY_PROTOCOL],"url":portMapping[api.KEY_SERVICE_URL],"port":portMapping[api.KEY_CONTAINER_PORT]}
+                            serviceObj["ports"].append(pm)
+
+    return dict(regions=regions)
 
 def port():
     region = request.args[0]
--- com/baoze/tenxcloud/api.py
+++ com/baoze/tenxcloud/api.py
@@ -5,6 +5,19 @@ import json
 import config
 from io import BytesIO
 
+KEY_REGIONS = "regions"
+KEY_REGION_NAME = "name"
+KEY_REGION_DISPLAY_MAME = "display_name"
+
+KEY_SERVICES = "services"
+KEY_SERVICE_REGION = "region"
+KEY_SERVICE_NAME = "name"
+
+KEY_PORT_MAPPING = "port_mapping"
+KEY_CONTAINER_PORT = "container_port"
+KEY_SERVICE_URL = "service_url"
+KEY_PROTOCOL = "protocol"
+
 def require(url):
         e = BytesIO()
         curl = pycurl.Curl()
@@ -18,6 +31,9 @@ def require(url):
 
         return json.loads(e.getvalue())
 
+def getRegions():
+    return require("https://api.tenxcloud.com/%s/regions" % (config.APIVERSION))
+
 def getServices(region):
     return require("https://api.tenxcloud.com/%s/regions/%s/services" % (config.APIVERSION,region))
 
@@ -27,16 +43,19 @@ def getService(region,service):
 def getInstances(region,service):
     return require("https://api.tenxcloud.com/%s/regions/%s/services/%s/instances" % (config.APIVERSION,region,service))
 
+def getMappedPortFromServiceUrl(url):
+    return url.split(":")[1]
+
 def getMappedPort(region,service,port):
     url = getMappedUrl(region,service,port)
-    return url.split(":")[1]
+    return getMappedPortFromServiceUrl(url)
 
 def getMappedUrl(region,service,port):
     ser = getService(region,service)
     url = None
-    portMappings = ser["port_mapping"]
+    portMappings = ser[KEY_PORT_MAPPING]
     for portMapping in portMappings :
-        if(int(port) == portMapping["container_port"]):
-            url = portMapping["service_url"]
+        if(int(port) == portMapping[KEY_CONTAINER_PORT]):
+            url = portMapping[KEY_SERVICE_URL]
             break
     return url
